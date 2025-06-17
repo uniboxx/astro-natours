b81a12d862748d778b86ee4101e060d6d6158c0c
unibox <unibox@duck.com>
2025-06-17 11:05:53 +0200

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://site_name.netlify.app',
+  site: 'https://astro-natours.netlify.app',
   integrations: [sitemap()],
 });
